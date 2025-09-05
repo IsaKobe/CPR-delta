eb6be7eabc4b41be2779f5f703f995649491eb2f
IsaKobe <risa.kopp@gmail.com>
2025-09-05 09:03:29 +0200

    Init

---
+++ src/main/java/cz/richard/Main.java
@@ -0,0 +1,15 @@
+package cz.richard;
+
+/**
+ * Main class
+ */
+public class Main {
+    /**
+     * entrypoint
+     *
+     * @param args cmd args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}

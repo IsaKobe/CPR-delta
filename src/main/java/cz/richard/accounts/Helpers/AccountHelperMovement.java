a595a9a78027413516ed5d8aedfac3503d5b9136
IsaKobe <risa.kopp@gmail.com>
2025-10-22 10:39:13 +0200

    Logger

--- src/main/java/cz/richard/accounts/Helpers/AccountHelperMovement.java
+++ src/main/java/cz/richard/accounts/Helpers/AccountHelperMovement.java
@@ -1,11 +1,13 @@
 package cz.richard.accounts.Helpers;
 
 import cz.richard.accounts.Data.Account;
+import cz.richard.accounts.Logger;
 import cz.richard.cards.Card;
 
 public class AccountHelperMovement {
     public static void withdraw(Account account, double amount) {
         if(AccountHelperValidator.validateWithdraw(account.getBalance(), amount)) {
+            Logger.getInstance().logWithdraw(account, amount);
             double newBalance = account.getBalance() - amount;
             account.setBalance(newBalance);
         }
@@ -13,6 +15,7 @@ public class AccountHelperMovement {
     public static void deposit(Account account, double amount) {
         if(AccountHelperValidator.validateDeposit(account.getBalance(), amount)){
             double newBalance = account.getBalance() + amount;
+            Logger.getInstance().logDeposit(account, amount);
             account.setBalance(newBalance);
         }
     }
---
+++ src/main/java/cz/richard/accounts/Logger.java
@@ -0,0 +1,26 @@
+package cz.richard.accounts;
+
+import cz.richard.accounts.Data.Account;
+
+import java.sql.Date;
+
+public class Logger {
+    static Logger instance;
+    private Logger() {}
+    public static Logger getInstance() {
+        if (instance == null)
+            instance = new Logger();
+        return instance;
+    }
+
+    public void logDeposit(Account account, double amount) {
+        log(String.format("deposit with account: %s, amount: %f", account, amount));
+    }
+    public void logWithdraw(Account account, double amount) {
+        log(String.format("withdraw with account: %s, amount: %f", account, amount));
+    }
+
+    void log(String message) {
+        System.out.printf("\n%s: %s", java.time.LocalDate.now(), message);
+    }
+}
